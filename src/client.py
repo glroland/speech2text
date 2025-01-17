c70756010e6f00fb035702e8af4dbe58e829cad8
Lee Roland <glroland@hotmail.com>
2025-01-16 20:55:51 -0500

    updated client to use remote server

--- src/client.py
+++ src/client.py
@@ -16,7 +16,8 @@ with open(audio_filename, "rb") as file:
     audio_file_binary = file.read()
 
 # Post soundbyte to service
-url = 'http://localhost:8000/transcribe/'
+url = 'https://speech2text-speech2text.apps.ocpprod.home.glroland.com/transcribe/'
+#url = 'http://localhost:8000/transcribe/'
 response = requests.post(url, files={"file": audio_file_binary})
 
 print (response.text)
